2f29eef7d67ef720acf3090802feb1f694aa2f3f
Mike Barr <m.barr@mac.com>
2025-05-05 21:05:38 +0200

    Refactor plant growth and water system, remove GDScript addon
    Removed the GDScript cheatsheet addon and its related files. Enhanced the plant growth system by integrating water level management and visual feedback directly into the plant.gd script. Added a water area system to hydrate plants and updated the environment with new water-related assets, including shaders and sound effects.

--- entities/plant/code/plant.gd
+++ entities/plant/code/plant.gd
@@ -1,18 +1,40 @@
 @icon("res://entities/plant/art/plant_icon.png")
-class_name Plant extends PhysicsBody3D
+class_name Plant
+extends PhysicsBody3D
 
+@onready var plant_sprite: Sprite3D = $Sprite3D
+
+@export_category("Growth")
 @export var is_growing: bool
 @export var growth_rate: float = 0.6
 @export var shrink_rate: float = -0.2
-@onready var plant_sprite: Sprite3D = $Sprite3D
+@export var size_min: float = 1.0
+@export var size_max: float = 6.5
 
-var target_min := 0.5
-var target_max := 6.5
+@export_category("Water")
+@export_range(0, 100, 5) var water_level: float
+@export var is_being_watered: bool
+@export var water_rate: float = 10
+@export var drying_rate: float = -2
+@export var water_min: float = 0.0
+@export var water_max: float = 100.0
+
+@export_category("Visuals")
+@export var healthy_color: Color = Color("#00b200")
+@export var unhealthy_color: Color = Color("#ff4b16")
+
 func _process(delta: float) -> void:
-	var change := delta * (growth_rate if is_growing else shrink_rate)
+	var growth := delta * (growth_rate if is_growing else shrink_rate)
+	plant_sprite.scale.x = clamp(plant_sprite.scale.x + growth, size_min, size_max)
+	plant_sprite.scale.y = clamp(plant_sprite.scale.y + growth, size_min, size_max)
 
-	plant_sprite.scale.x = clamp(plant_sprite.scale.x + change, target_min, target_max)
-	plant_sprite.scale.y = clamp(plant_sprite.scale.y + change, target_min, target_max)
+	water_level = clamp(water_level + delta * (water_rate if is_being_watered else drying_rate), water_min, water_max)
+
+	var plant_color: float = clamp(water_level / 100.0, 0.0, 1.0)
+	plant_sprite.modulate = healthy_color.lerp(unhealthy_color, 1.0 - plant_color)
+
+func set_watered(watered: bool) -> void:
+	is_being_watered = watered
 
 func set_growing(growing: bool) -> void:
 	is_growing = growing
---
+++ environment/code/water_area.gd
@@ -0,0 +1,11 @@
+extends Area3D
+
+
+func _on_plant_entered(body: Node3D) -> void:
+	if body is Plant:
+		body.set_watered(true)
+
+
+func _on_plant_exited(body: Node3D) -> void:
+	if body is Plant:
+		body.set_watered(false)

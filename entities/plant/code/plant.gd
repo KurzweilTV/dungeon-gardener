c9212a297fc1dcd064661aea30c2d8dd0984fdc3
Mike Barr <m.barr@mac.com>
2025-05-06 10:30:50 +0200

    Adds dirt texture and plant wetness

--- entities/plant/code/plant.gd
+++ entities/plant/code/plant.gd
@@ -3,9 +3,11 @@ class_name Plant
 extends PhysicsBody3D
 
 @onready var plant_sprite: Sprite3D = $Sprite3D
+@onready var pot: MeshInstance3D = %Pot
 
 @export_category("Growth")
 @export var is_growing: bool
+@export var is_max_growth: bool
 @export var growth_rate: float = 0.6
 @export var shrink_rate: float = -0.2
 @export var size_min: float = 1.0
@@ -19,22 +21,36 @@ extends PhysicsBody3D
 @export var water_min: float = 0.0
 @export var water_max: float = 100.0
 
+
 @export_category("Visuals")
 @export var healthy_color: Color = Color("#00b200")
 @export var unhealthy_color: Color = Color("#ff4b16")
 
 func _process(delta: float) -> void:
-	var growth := delta * (growth_rate if is_growing else shrink_rate)
-	plant_sprite.scale.x = clamp(plant_sprite.scale.x + growth, size_min, size_max)
-	plant_sprite.scale.y = clamp(plant_sprite.scale.y + growth, size_min, size_max)
-
-	water_level = clamp(water_level + delta * (water_rate if is_being_watered else drying_rate), water_min, water_max)
-
+	if !is_max_growth:
+		var growth := delta * (growth_rate if is_growing else shrink_rate)
+		plant_sprite.scale.x = clamp(plant_sprite.scale.x + growth, size_min, size_max)
+		plant_sprite.scale.y = clamp(plant_sprite.scale.y + growth, size_min, size_max)
+		if plant_sprite.scale.x >= size_max:
+			set_max_growth()
+		
 	var plant_color: float = clamp(water_level / 100.0, 0.0, 1.0)
+	water_level = clamp(water_level + delta * (water_rate if is_being_watered else drying_rate), water_min, water_max)
 	plant_sprite.modulate = healthy_color.lerp(unhealthy_color, 1.0 - plant_color)
-
+	set_soil_wetness(plant_color)
+	
+func set_soil_wetness(plant_color: float) -> void:
+	var surface_material := pot.get_surface_override_material(1)
+	if surface_material:
+		surface_material.roughness = lerp(1.0, 0.0, plant_color)
+	
 func set_watered(watered: bool) -> void:
 	is_being_watered = watered
 
 func set_growing(growing: bool) -> void:
 	is_growing = growing
+
+func set_max_growth() -> void:
+	is_max_growth = true
+	print("Max Growth")
+	# TODO: Trigger particles
--- environment/code/light_shaft.gd
+++ environment/code/light_shaft.gd
@@ -2,10 +2,8 @@ extends FogVolume
 
 func _on_light_entered(body: Node3D) -> void:
 	if body is Plant:
-		print_rich("[color=green]Plant is growing!")
 		body.set_growing(true)
 
 func _on_light_exited(body: Node3D) -> void:
 	if body is Plant:
-		print_rich("[color=red]Plant is not growing")
 		body.set_growing(false)
